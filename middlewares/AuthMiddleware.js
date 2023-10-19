755e63f8bbf7b0b176bd16527688192ce00195ff
Ossama kharbaq <ossamakharbaq4@gmail.com>
2023-10-19 11:55:48 +0100

    AL-5 log out finished and also the refresh token has been implemented

--- middlewares/AuthMiddleware.js
+++ middlewares/AuthMiddleware.js
@@ -2,20 +2,43 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 async function verifyLocalToken(req,res,next){
-    const token = req.cookies.accessToken;
-    if(!token) return res.status(403).json({message:"NO TOKEN , Action denied"})
-    if(token == null || token == undefined) return res.status(403).json({message:"NO TOKEN Provided"})
-
-    if(token){
-        let user = jwt.verify(token, process.env.JWT_SECRET)
-        user = await User.findById(user.id)
-        console.log(user);
-        if(!user) return res.status(403).json({message:"invalid Token"})
-        req.user = user
-        next()
-    }else{
-        return res.status(403).json({message:"NO TOKEN Provided wrongggg secret"})
+    const accessToken = req.cookies['accessToken'];
+    const refreshToken = req.cookies['refreshToken'];
+    let user;
+    if (!accessToken || !refreshToken) {
+        return res.status(401).json({ message: 'Action denied' });
     }
+    try {
+        user = jwt.verify(accessToken, process.env.JWT_SECRET);
+        req.user = user;
+        next();
+      } catch (err) {
+        console.log(err);
+        try {
+          user = jwt.verify(refreshToken, process.env.JWT_REFRESH);
+          const newAccessToken = jwt.sign(
+            {
+                id: user.id,
+                email: user.email,
+                full_name: user.full_name,
+                phone_number: user.phone_number,
+                address: user.address ,
+            }
+            , process.env.JWT_SECRET, {
+            expiresIn: 900,
+          });
+      
+          res.cookie('accessToken', newAccessToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'Strict',
+          });
+          next();
+        } catch (err) {
+          res.status(401).json({ message: 'Invalid access and refresh tokens',user });
+        }
+      }
+
 }
 
 async function verifyMailedToken(req,res,next){
