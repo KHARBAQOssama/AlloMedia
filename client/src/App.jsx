00fb9557c1821752a86cfdb8733b8848e0f4b32c
Ossama kharbaq <ossamakharbaq4@gmail.com>
2023-10-28 22:14:14 +0100

    i have successfully consume the api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,40 @@
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
 
-import './App.css'
-import LandingPage from './views/LandingPage';
-import RootLayout from './views/RootLayout';
-import Login from './views/signPages/Login';
-import SignUp from './views/signPages/SignUp';
+// import './App.css'
+import LandingPage from "./views/pages/LandingPage";
+import RootLayout from "./views/RootLayout";
+import Login from "./views/signPages/Login";
+import SignUp from "./views/signPages/SignUp";
+import RegisterSuccess from "./views/pages/RegisterSuccess";
+import { useUser } from "./contexts/userContext";
+import Profile from "./views/pages/Profile";
+import VerifyEmail from "./views/pages/VerifyEmail";
+import ResetPassword from "./views/pages/ResetPassword";
+import ForgetPassword from "./views/pages/ForgetPassword";
 
-
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<RootLayout/>}>
-      <Route path='/' element={<LandingPage/>}>
-        <Route path='login' element={<Login/>}/>
-        <Route path='register' element={<SignUp/>}/>
-      </Route>
-    </Route>
-  )
-) 
 function App() {
-  
-  return (
-
-      <RouterProvider router={router}/>
-  )
+  const {user} = useUser();
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout/>}>
+        <Route path="/" element={<LandingPage/>} />
+        <Route path="/login" element={!user._id ? <Login/> : <Navigate to={'/me'}/>} />
+        <Route path="/register" element={!user._id ? <SignUp/> : <Navigate to={'/me'}/>} />
+        <Route path="/registerSuccess" element={<RegisterSuccess/>}/>
+        <Route path="/me" element={user._id ? <Profile/> : <Navigate to={'/login'}/>}/>
+        <Route path="/verifyEmail" element={<VerifyEmail/>}/>
+        <Route path="/resetPassword" element={<ResetPassword/>}/>
+        <Route path="/forgetPassword" element={<ForgetPassword/>}/>
+      </Route>
+    )
+  );
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
--- client/src/schemas/AuthForms.js
+++ client/src/schemas/AuthForms.js
@@ -1,13 +1,37 @@
-import * as yup from 'yup';
+import * as yup from "yup";
 
 export const registerSchema = yup.object().shape({
-    email : yup.string().email("Please enter a valid email").required("email is required"),
-    full_name : yup.string().required("full name is required"),
-    password : yup.string().min(6).required("Password is required"),
-    password_confirmation : yup.string().oneOf([yup.ref('password'),null],"Passwords do not match").required("Password is required")
-})
+  email: yup
+    .string()
+    .email("Please enter a valid email")
+    .required("email is required"),
+  full_name: yup.string().required("full name is required"),
+  password: yup.string().min(6).required("Password is required"),
+  password_confirmation: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Passwords do not match")
+    .required("Password is required"),
+});
 
 export const loginSchema = yup.object().shape({
-    email : yup.string().email("Please enter a valid email").required("email is required"),
-    password : yup.string().min(6).required("Password is required"),
-})  
+  email: yup
+    .string()
+    .email("Please enter a valid email")
+    .required("email is required"),
+  password: yup.string().min(6).required("Password is required"),
+});
+
+export const forgetPasswordSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Please enter a valid email")
+    .required("email is required"),
+});
+
+export const resetPasswordSchema = yup.object().shape({
+  password: yup.string().min(6).required("Password is required"),
+  password_confirmation: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Passwords do not match")
+    .required("Password is required"),
+});
